ab8d6e00db08d01086b9efc3d3ab4862f81e3ad2
Julio Mendoza <jucem@linux.site>
2015-05-10 20:23:54 -0500

    Adding Login New page

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -4,12 +4,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Login extends MY_Controller {
 
 	/**
-	 * Default Action /home
+	 * Default Action /login
 	 */
 	public function index()
 	{
 		$data['title'] = 'Coherencia | Inicio de Sesi&oacute;n';
 		$data['view_styles'] = $this->parse_view('auth/login_styles');
 		$this->to_main_view('auth/login', $data);
+	}
+
+	/**
+	 * Action /login/new
+	 */
+	public function create()
+	{
+		$this->to_main_view('auth/new');
 	}
 }
